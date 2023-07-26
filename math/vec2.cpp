1a3cb936eafc8ba19a9c7d7b217ee9fc261666fe
zdamm001 <zdamm001@ucr.edu>
2023-07-26 03:03:11 -0700

    finished vec2

--- math/vec2.cpp
+++ math/vec2.cpp
@@ -1,4 +1,7 @@
 #include <cmath>
+#include <string>
+
+using namespace std;
 
 class vec2 {
     public:
@@ -15,7 +18,69 @@ class vec2 {
             return vec2(end.x - start.x, end.y - start.y);
         }
 
-        vec2 VecPerp(const vec2& vec) {
-            return vec2(-vec.y, vec.x);
+        vec2 VecPerp(const vec2& v) {
+            return vec2(-v.y, v.x);
+        }
+
+        double VecDot(const vec2& v1, const vec2& v2) {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        vec2 To(const vec2& end) {
+            return vec2(end.x - x, end.y - y);
+        }
+
+        vec2 Perp() {
+            return vec2(-y, x);
+        }
+
+        double PerpDot(const vec2& v) {
+            return -y * v.x + x * v.y;
+        }
+
+        vec2 Clone() {
+            return vec2(x, y);
+        }
+
+        vec2 Plus(const vec2& v) {
+            return vec2(x + v.x, y + v.y);
+        }
+
+        vec2 Minus(const vec2& v) {
+            return vec2(x - v.x, y - v.y);
+        }
+
+        double Dot(const vec2& v) {
+            return x * v.x + y * v.y;
+        }
+
+        double Len() {
+            return sqrt(x * x + y * y);
+        }
+
+        double LenSq() {
+            return x * x + y * y;
+        }
+
+        string ToString() {
+            return "(" + to_string(x) + "," + to_string(y) + ")";
+        }
+
+        void Copy(const vec2& v) {
+            x = v.x;
+            y = v.y;
+        }
+
+        void Normalize() {
+            double length = sqrt(x * x + y * y);
+            if (length != 0) {
+                x /= length;
+                y /= length;
+            }
+        }
+
+        void Scale(double scalar) {
+            x *= scalar;
+            y *= scalar;
         }
 };
