9d56352f4652120a183738f8f470054ed10a96a8
zdamm001 <zdamm001@ucr.edu>
2023-07-27 19:36:37 -0700

    started base64 with encode function

---
+++ com/foxarc/util/Base64.cpp
@@ -0,0 +1,48 @@
+#include "Base64.h"
+
+const vector<char> Base64::encodeChars = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+const vector<int> Base64::decodeChars = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1,
+    -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+    50, 51, -1, -1, -1, -1, -1
+};
+
+string Base64::encode(const ByteArray& data) {
+    vector<char> result;
+    int byteIndex = 0;
+    int length = data.length();
+    int paddingBytes = length % 3;
+    int bytesInGroupsOfThree = length - paddingBytes;
+    while (byteIndex < bytesInGroupsOfThree) {
+        int val = (data[byteIndex++] << 16) | (data[byteIndex++] << 8) | data[byteIndex++];
+        result.push_back(encodeChars[val >> 18]);
+        result.push_back(encodeChars[(val >> 12) & 63]);
+        result.push_back(encodeChars[(val >> 6) & 63]);
+        result.push_back(encodeChars[val & 63]);
+    }
+    if (paddingBytes == 1) {
+        int val = data[byteIndex++];
+        result.push_back(encodeChars[val >> 2]);
+        result.push_back(encodeChars[(val & 3) << 4]);
+        result.push_back('=');
+        result.push_back('=');
+    }
+    else if (paddingBytes == 2) {
+        int val = (data[byteIndex++] << 8) | data[byteIndex++];
+        result.push_back(encodeChars[val >> 10]);
+        result.push_back(encodeChars[(val >> 4) & 63]);
+        result.push_back(encodeChars[(val & 15) << 2]);
+        result.push_back('=');
+    }
+    return string(result.begin(), result.end());
+}
---
+++ com/foxarc/util/Base64.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "..\\..\\..\\flash\\utils\\ByteArray.h"
+
+using namespace std;
+
+class Base64 {
+    private:
+        static const vector<char> encodeChars;
+        static const vector<int> decodeChars;
+    public:
+        static string encode(const ByteArray& data);
+
+};
