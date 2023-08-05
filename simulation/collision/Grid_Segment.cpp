f9dcd5942dd05c98cf6e7c1e9bd0079c4a63489e
zdamm001 <zdamm001@ucr.edu>
2023-08-05 14:45:20 -0700

    completed Grid_Segment

--- simulation/collision/Grid_Segment.cpp
+++ simulation/collision/Grid_Segment.cpp
@@ -82,3 +82,129 @@ double Grid_Segment::GetRaycastDistance(double pos_x, double pos_y, double dir_x
     }
     return (hit_t * HACKY_LEN);
 }
+
+bool Grid_Segment::RaycastVsPlayer(const vec2& query_pos, const vec2& player_pos, double player_r, vec2& hit_pos, vec2& hit_n) {
+    double dir_to_player_X = player_pos.x - query_pos.x;
+    double dir_to_player_Y = player_pos.y - query_pos.y;
+    
+    double dist_to_player = sqrt(dir_to_player_X * dir_to_player_X + dir_to_player_Y * dir_to_player_Y);
+    
+    if (dist_to_player < player_r) {
+        return true;
+    }
+    
+    dir_to_player_X /= dist_to_player;
+    dir_to_player_Y /= dist_to_player;
+        
+    dist_to_player -= player_r;
+        
+    double hit_dist = GetRaycastDistance(query_pos.x, query_pos.y, dir_to_player_X, dir_to_player_Y, hit_pos, hit_n);
+    
+    if (dist_to_player < hit_dist) {
+        return true;
+    }
+    
+    if (hit_dist == -1) {
+        hit_pos.x = 0;
+        hit_pos.y = 0;
+        hit_n.x = 1;
+        hit_n.y = 0;
+    }
+    
+    return false;
+}
+
+double Grid_Segment::IntersectRayVsCellContents(int u, int v, const vec2& ray_pos, const vec2& ray_vec, vec2& out_pos, vec2& out_normal) {
+    double best_t = 2;
+    vec2 TEMP_temp_p;
+    vec2 TEMP_temp_n;
+    
+    TEMP_temp_p.x = 0;
+    TEMP_temp_p.y = 0;
+    TEMP_temp_n.x = 0;
+    TEMP_temp_n.y = 0;
+    
+    int index = GetCellIndexFromGridspacePosition(u, v);
+    const vector<Segment>& segList = cells[index];
+    
+    for (size_t i = 0; i < segList.size(); i++) {
+        const Segment& seg = segList[i];
+        double curr_t = seg.IntersectWithRay(ray_pos, ray_vec, 0, TEMP_temp_p, TEMP_temp_n);
+        
+        if (curr_t == -1) {
+            return -1;
+        } else if (curr_t < best_t) {
+            best_t = curr_t;
+            out_pos.Copy(TEMP_temp_p);
+            out_normal.Copy(TEMP_temp_n);
+        }
+    }
+    
+    return best_t;
+}
+
+vector<Segment> Grid_Segment::DEBUG_GetCellContentsFromGridspacePosition(int u, int v) {
+    int index = GetCellIndexFromGridspacePosition(u, v);
+    return cells[index];
+}
+
+void Grid_Segment::Clear() {
+    for (int i = 0; i < numcells; i++) {
+        cells[i].clear();
+    }
+}
+
+void Grid_Segment::AddSegToCell(int cell_u, int cell_v, Segment& seg) {
+    int index = GetCellIndexFromGridspacePosition(cell_u, cell_v);
+    if (index < 0 || index >= cells.size()) {
+        return;
+    }
+    cells[index].push_back(seg);
+}
+
+void Grid_Segment::DOOR_AddSegment(int cell_index, Segment& seg) {
+    if (cell_index < 0 || cell_index >= numcells) {
+        return;
+    }
+    vector<Segment>& cell = cells[cell_index];
+    if (find(cell.begin(), cell.end(), seg) != cell.end()) {
+        return;
+    }
+    cells[cell_index].push_back(seg);
+}
+
+void Grid_Segment::DOOR_RemoveSegment(int cell_index, Segment& seg) {
+    if (cell_index < 0 || cell_index >= numcells) {
+        return;
+    }
+    vector<Segment>& cell = cells[cell_index];
+    vector<Segment>::iterator it = find(cell.begin(), cell.end(), seg);
+    if (it == cell.end()) {
+        return;
+    }
+    cell.erase(it);
+}
+
+int Grid_Segment::DOOR_GetCellIndexFromGridspacePosition(int u, int v) {
+    return GetCellIndexFromGridspacePosition(u, v);
+}
+
+void Grid_Segment::GatherCellContentsFromWorldspaceRegion(double min_x, double min_y, double max_x, double max_y, vector<Segment>& out_segList) {
+    int min_u = WorldspaceToGridspace(min_x);
+    int max_u = WorldspaceToGridspace(max_x);
+    int min_v = WorldspaceToGridspace(min_y);
+    int max_v = WorldspaceToGridspace(max_y);
+
+    out_segList.clear();
+
+    for (int j = min_v; j <= max_v; j++) {
+        for (int i = min_u; i <= max_u; i++) {
+            int index = GetCellIndexFromGridspacePosition(i, j);
+            if (index < 0) {
+                //continue;
+            }
+            vector<Segment>& cell = cells[index];
+            out_segList.insert(out_segList.end(), cell.begin(), cell.end());
+        }
+    }
+}
--- simulation/collision/Grid_Segment.h
+++ simulation/collision/Grid_Segment.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 #include "Grid_Base.h"
 #include "Segment.h"
@@ -15,4 +16,15 @@ class Grid_Segment : public Grid_Base {
         Grid_Segment(int num_cols, int num_rows, double cell_size);
         void DEBUG_Draw(SimpleRenderer& rend);
         double GetRaycastDistance(double pos_x, double pos_y, double dir_x, double dir_y, vec2& out_pos, vec2& out_normal);
+        bool RaycastVsPlayer(const vec2& query_pos, const vec2& player_pos, double player_r, vec2& hit_pos, vec2& hit_n);
+    private:
+        double IntersectRayVsCellContents(int u, int v, const vec2& ray_pos, const vec2& ray_vec, vec2& out_pos, vec2& out_normal);
+    public:
+        vector<Segment> DEBUG_GetCellContentsFromGridspacePosition(int u, int v);
+        void Clear();
+        void AddSegToCell(int cell_u, int cell_v, Segment& seg);
+        void DOOR_AddSegment(int cell_index, Segment& seg);
+        void DOOR_RemoveSegment(int cell_index, Segment& seg);
+        int DOOR_GetCellIndexFromGridspacePosition(int u, int v);
+        void GatherCellContentsFromWorldspaceRegion(double min_x, double min_y, double max_x, double max_y, vector<Segment>& out_segList);
 };
