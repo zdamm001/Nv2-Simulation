4372c2582dec0f69706f97918c2b6e1a80051ca4
zdamm001 <zdamm001@ucr.edu>
2023-08-05 04:50:25 -0700

    added Segment base class

---
+++ simulation/collision/Segment.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "AABB.h"
+#include "..\\..\\math\\vec2.h"
+
+class SimpleRenderer;
+
+class Segment {
+    public:
+        virtual ~Segment() = default;
+        virtual AABB GetAABB() = 0;
+        virtual void GetClosestPoint(const vec2& query_pos, vec2& out_closestpoint) = 0;
+        virtual double IntersectWithRay(const vec2& ray_pos, const vec2& ray_vec, double ray_radius, vec2& OUT_intersection_pos, vec2& OUT_intersection_normal) = 0;
+        virtual bool GetClosestPoint_IsBackfacing(const vec2& query_pos, vec2& out_closestpoint) = 0;
+        virtual void DebugDraw(SimpleRenderer& rend) = 0;
+        virtual void DebugDraw_NoStyle(SimpleRenderer& rend) = 0;
+        virtual void DebugDraw_Simple(SimpleRenderer& rend) = 0;
+};
