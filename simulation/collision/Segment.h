888538f06ed131dfd5ca432d33fff308c1ae29c0
zdamm001 <zdamm001@ucr.edu>
2023-08-05 14:44:28 -0700

    added const to some functions

--- simulation/collision/Segment.h
+++ simulation/collision/Segment.h
@@ -8,10 +8,10 @@ class SimpleRenderer;
 class Segment {
     public:
         virtual ~Segment() = default;
-        virtual AABB GetAABB() = 0;
-        virtual void GetClosestPoint(const vec2& query_pos, vec2& out_closestpoint) = 0;
-        virtual double IntersectWithRay(const vec2& ray_pos, const vec2& ray_vec, double ray_radius, vec2& OUT_intersection_pos, vec2& OUT_intersection_normal) = 0;
-        virtual bool GetClosestPoint_IsBackfacing(const vec2& query_pos, vec2& out_closestpoint) = 0;
+        virtual AABB GetAABB() const = 0;
+        virtual void GetClosestPoint(const vec2& query_pos, vec2& out_closestpoint) const = 0;
+        virtual double IntersectWithRay(const vec2& ray_pos, const vec2& ray_vec, double ray_radius, vec2& OUT_intersection_pos, vec2& OUT_intersection_normal) const = 0;
+        virtual bool GetClosestPoint_IsBackfacing(const vec2& query_pos, vec2& out_closestpoint) const = 0;
         virtual void DebugDraw(SimpleRenderer& rend) = 0;
         virtual void DebugDraw_NoStyle(SimpleRenderer& rend) = 0;
         virtual void DebugDraw_Simple(SimpleRenderer& rend) = 0;
