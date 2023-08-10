b1c3dae1b464b4294d89fb3e0b8ba17f73314e18
zdamm001 <zdamm001@ucr.edu>
2023-08-10 03:41:33 -0700

    added edgetypes

---
+++ tiles/edgetypes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class edgetypes {
+    public:
+        static const int EMPTY = 0;
+        static const int PARTIAL = 1;
+        static const int SOLID = 2;
+        edgetypes() = delete;
+};
