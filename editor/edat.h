0e1b1a064e467b20c86e1493c19cef7ac43d2c72
zdamm001 <zdamm001@ucr.edu>
2023-08-11 03:44:03 -0700

    finished edat

--- editor/edat.h
+++ editor/edat.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <vector>
+
+#include "..\\math\\vec2.h"
+
+using namespace std;
 
 class edat {
     public:
@@ -51,5 +56,40 @@ class edat {
         static const unsigned int MOVE_SURFACE_CCW = 1;
         static const unsigned int MOVE_WANDER_CW = 2;
         static const unsigned int MOVE_WANDER_CCW = 3;
+
+        static vector<unsigned int> HELPER_MIRROR_MOVE;
+        static vector<unsigned int> MAP_TILETYPE_TO_TILEID;
+        //static vector<Class> MAP_ETYPE_TO_MC;
+        //static vector<Class> MAP_ETYPE_TO_MC_SELECTED;
+        static vector<vec2> HELPER_MAP_DIR_TO_VEC;
+        static vector<vec2> HELPER_MAP_ETYPE_TO_COLSHAPE;
+
+        static const unsigned int STRUCTTYPE_PLAYER = 0;
+        static const unsigned int STRUCTTYPE_MINE = 1;
+        static const unsigned int STRUCTTYPE_GOLD = 2;
+        static const unsigned int STRUCTTYPE_EXIT = 3;
+        static const unsigned int STRUCTTYPE_DOOR_REGULAR = 4;
+        static const unsigned int STRUCTTYPE_DOOR_LOCKED = 5;
+        static const unsigned int STRUCTTYPE_DOOR_TRAP = 6;
+        static const unsigned int STRUCTTYPE_LAUNCHPAD = 7;
+        static const unsigned int STRUCTTYPE_ONEWAY = 8;
+        static const unsigned int STRUCTTYPE_CHAINGUN = 9;
+        static const unsigned int STRUCTTYPE_LASER = 10;
+        static const unsigned int STRUCTTYPE_ZAP = 11;
+        static const unsigned int STRUCTTYPE_CHASER = 12;
+        static const unsigned int STRUCTTYPE_FLOORGUARD = 13;
+        static const unsigned int STRUCTTYPE_BOUNCEBLOCK = 14;
+        static const unsigned int STRUCTTYPE_ROCKET = 15;
+        static const unsigned int STRUCTTYPE_TURRET = 16;
+        static const unsigned int STRUCTTYPE_THWOMP = 17;
+        static const unsigned int num_structtypes = 18;
+
+        static vector<unsigned int> STRUCT_SIZE;
+        static vector<unsigned int> MAP_STRUCT_TO_ETYPE;
+    public:
         edat() = delete;
+        static void Initialize();
+        static vec2 GET_WORLDSPACE_POS(const vector<unsigned int>& data);
+        static vec2 MAP_DIR_TO_VEC(int direction);
+        static vec2 GET_COLLISION_SHAPE(const vector<unsigned int>& data);
 };
