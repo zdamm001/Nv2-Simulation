1f7e160156d5bc1661e095e894835ce5fc68341b
zdamm001 <zdamm001@ucr.edu>
2023-08-15 03:26:01 -0700

    added all important sim_loader functions

--- simulation/sim_loader.cpp
+++ simulation/sim_loader.cpp
@@ -44,3 +44,300 @@ Simulator sim_loader::LoadLevel_EditorState(const vector<int>& playerKeys, const
 
     return Simulator(tileIDGrid, gridSegment, gridEdges, gridEntity, entities, ninjas);
 }
+
+void sim_loader::LoadLevel_EditorState_Tiles(const vector<unsigned int>& tileIDs, vector<int>& tileIDGrid, Grid_Segment& gridSegment, Grid_Edges& gridEdges, int numCols, int numRows, double cellSize, double cellHalfWidth) {
+    gridSegment.Clear();
+    gridEdges.Clear();
+    LoadLevel_InitTileIDGridWithBoundaryEdges(tileIDGrid, numCols, numRows);
+
+    int currentIndex = 0;
+    while (currentIndex < tileIDs.size()) {
+        int colIndex = 1 + currentIndex % edat::num_cols;
+        int rowIndex = 1 + currentIndex / edat::num_cols;
+        tileIDGrid[colIndex + rowIndex * numCols] = tileIDs[currentIndex];
+        currentIndex++;
+    }
+
+    vector<int> neighborTiles(4, tiletypes::EMPTY);
+    for (int gridIndex = 0; gridIndex < tileIDGrid.size(); ++gridIndex) {
+        int col = gridIndex % numCols;
+        int row = gridIndex / numCols;
+
+        if (col == 0) {
+            neighborTiles[0] = tiletypes::EMPTY;
+        } else {
+            neighborTiles[0] = tileIDGrid[col - 1 + row * numCols];
+        }
+
+        if (col == numCols - 1) {
+            neighborTiles[2] = tiletypes::EMPTY;
+        } else {
+            neighborTiles[2] = tileIDGrid[col + 1 + row * numCols];
+        }
+
+        if (row == 0) {
+            neighborTiles[3] = tiletypes::EMPTY;
+        } else {
+            neighborTiles[3] = tileIDGrid[col + (row - 1) * numCols];
+        }
+
+        if (row == numRows - 1) {
+            neighborTiles[1] = tiletypes::EMPTY;
+        } else {
+            neighborTiles[1] = tileIDGrid[col + (row + 1) * numCols];
+        }
+
+        LoadLevel_BuildTileSegs(gridSegment, cellSize, cellHalfWidth, col, row, tileIDGrid[gridIndex], neighborTiles);
+        LoadLevel_BuildTileEdges(gridEdges, col, row, tileIDGrid[gridIndex]);
+    }
+}
+
+void LoadLevel_InitTileIDGridWithBoundaryEdges(vector<int>& tileIDGrid, int numCols, int numRows) {
+    int numCells = numCols * numRows;
+
+    for (int i = 0; i < numCells; ++i) {
+        tileIDGrid[i] = tiletypes::EMPTY;
+    }
+
+    for (int col = 1; col < numCols - 1; ++col) {
+        tileIDGrid[col] = tiletypes::EDGE_BOTTOM;
+        tileIDGrid[col + (numRows - 1) * numCols] = tiletypes::EDGE_TOP;
+    }
+
+    for (int row = 1; row < numRows - 1; ++row) {
+        tileIDGrid[row * numCols] = tiletypes::EDGE_RIGHT;
+        tileIDGrid[numCols - 1 + row * numCols] = tiletypes::EDGE_LEFT;
+    }
+
+    tileIDGrid[0] = tiletypes::EDGE_CORNER_UL;
+    tileIDGrid[numCols - 1] = tiletypes::EDGE_CORNER_UR;
+    tileIDGrid[(numRows - 1) * numCols] = tiletypes::EDGE_CORNER_DL;
+    tileIDGrid[numCols * numRows - 1] = tiletypes::EDGE_CORNER_DR;
+}
+
+void sim_loader::LoadLevel_BuildTileSegs(Grid_Segment& gridSegment, double cellSize, double cellHalfWidth, int colIndex, int rowIndex, int tileType, const vector<int>& neighborTiles) {
+    vector<Segment> segments = tiledefs::GenerateTileSegments_Filtered(tileType, neighborTiles, colIndex * cellSize + cellHalfWidth, rowIndex * cellSize + cellHalfWidth, cellHalfWidth);
+    
+    for (size_t i = 0; i < segments.size(); ++i) {
+        gridSegment.AddSegToCell(colIndex, rowIndex, segments[i]);
+    }
+}
+
+void sim_loader::LoadLevel_BuildTileEdges(Grid_Edges& gridEdges, int colIndex, int rowIndex, int tileType) {
+    gridEdges.GAME_LoadTileEdges(colIndex, rowIndex, tileType);
+}
+
+void sim_loader::LoadLevel_EditorState_Entities(const vector<vector<unsigned int>>& entityData, Grid_Segment& gridSegment, Grid_Edges& gridEdges, Grid_Entity& gridEntity, vector<Entity_Base*>& entities, vector<vec2>& ninjaSpawnLocations, double cellSize, double cellHalfWidth) {
+    unsigned int entityType;
+    double entityX;
+    double entityY;
+    unsigned int entityDir;
+    unsigned int entityMove;
+
+    gridEntity.Clear();
+    entities.clear();
+    ninjaSpawnLocations.clear();
+
+    for (size_t i = 0; i < entityData.size(); ++i) {
+        const vector<unsigned int>& entityProps = entityData[i];
+        entityType = entityProps[edat::EPROP_TYPE];
+        entityX = entityProps[edat::EPROP_X] * edat::quantize_step_size;
+        entityY = entityProps[edat::EPROP_Y] * edat::quantize_step_size;
+        entityDir = 0;
+        entityMove = 0;
+
+        if (entityProps.size() > 3) {
+            entityDir = entityProps[edat::EPROP_DIR];
+            if (entityProps.size() > 4) {
+                entityMove = entityProps[edat::EPROP_MOVE];
+            }
+        }
+
+        if (entityType == edat::ETYPE_BOUNCEBLOCK) {
+            Entity_BounceBlock* bounceBlock = new Entity_BounceBlock(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, bounceBlock);
+        }
+        else if (entityType == edat::ETYPE_CHAINGUN) {
+            Entity_Drone_Chaingun* chaingunDrone = new Entity_Drone_Chaingun(gridEntity, entityX, entityY, Helper_Editor_NewDirEnumToOldDirEnum(entityDir), entityMove);
+            Helper_RegisterEntity(entities, chaingunDrone);
+        }
+        else if (entityType == edat::ETYPE_CHASER) {
+            Entity_Drone_Chaser* chaserDrone = new Entity_Drone_Chaser(gridEntity, entityX, entityY, Helper_Editor_NewDirEnumToOldDirEnum(entityDir), entityMove);
+            Helper_RegisterEntity(entities, chaserDrone);
+        }
+        else if (entityType == edat::ETYPE_DOOR_LOCKED || entityType == edat::ETYPE_DOOR_REGULAR || entityType == edat::ETYPE_DOOR_TRAP) {
+            vec2 directionVector = edat::MAP_DIR_TO_VEC(entityDir);
+            int gridX = static_cast<int>(floor((entityX - directionVector.x * 12) / 24));
+            int gridY = static_cast<int>(floor((entityY - directionVector.y * 12) / 24));
+            int cellIndex = gridSegment.DOOR_GetCellIndexFromGridspacePosition(gridX, gridY);
+            bool isVertical = (entityDir == 0);
+            vector<int> edgeCells(2, 0);
+
+            if (!isVertical) {
+                edgeCells[0] = gridEdges.DOOR_GetCellIndexFromGridspacePosition(gridX * 2, gridY * 2 + 1);
+                edgeCells[1] = gridEdges.DOOR_GetCellIndexFromGridspacePosition(gridX * 2 + 1, gridY * 2 + 1);
+            } else {
+                edgeCells[0] = gridEdges.DOOR_GetCellIndexFromGridspacePosition(gridX * 2 + 1, gridY * 2);
+                edgeCells[1] = gridEdges.DOOR_GetCellIndexFromGridspacePosition(gridX * 2 + 1, gridY * 2 + 1);
+            }
+
+            vec2 cellCenterPosition = gridSegment.DEBUG_GetWorldspaceCellCenterPositionFromIndex(cellIndex);
+            vec2 entityPosition(entityX, entityY);
+            vec2 perpendicularVector = directionVector.Perp();
+            perpendicularVector.Scale(12);
+            vec2 sideA = entityPosition.Plus(perpendicularVector);
+            vec2 sideB = entityPosition.Minus(perpendicularVector);
+            Segment_Linear_DoubleSided doorSegment(sideA.x, sideA.y, sideB.x, sideB.y);
+
+            if (entityType == edat::ETYPE_DOOR_REGULAR) {
+                Entity_Door_Regular* regularDoor = new Entity_Door_Regular(gridEntity, gridSegment, cellIndex, doorSegment, gridEdges, edgeCells, isVertical, entityX, entityY);
+                Helper_RegisterEntity(entities, regularDoor);
+            } else {
+                vector<unsigned int> nextEntityData = entityData[i + 1];
+                unsigned int nextEntityX = nextEntityData[edat::EPROP_X] * edat::quantize_step_size;
+                unsigned int nextEntityY = nextEntityData[edat::EPROP_Y] * edat::quantize_step_size;
+
+                if (entityType == edat::ETYPE_DOOR_LOCKED) {
+                    Entity_Door_Locked* lockedDoor = new Entity_Door_Locked(gridEntity, gridSegment, cellIndex, doorSegment, gridEdges, edgeCells, isVertical, nextEntityX, nextEntityY);
+                    Helper_RegisterEntity(entities, lockedDoor);
+                }
+                else if (entityType == edat::ETYPE_DOOR_TRAP) {
+                    Entity_Door_Trap* trapDoor = new Entity_Door_Trap(gridEntity, gridSegment, cellIndex, doorSegment, gridEdges, edgeCells, isVertical, nextEntityX, nextEntityY);
+                    Helper_RegisterEntity(entities, trapDoor);
+                }
+            }
+        }
+        else if (entityType == edat::ETYPE_EXIT_DOOR) {
+            Entity_ExitDoor* exitDoor = new Entity_ExitDoor(entityX, entityY);
+            Helper_RegisterEntity(entities, exitDoor);
+
+            vector<unsigned int> nextEntityData = entityData[i + 1];
+            unsigned int nextEntityX = nextEntityData[edat::EPROP_X] * edat::quantize_step_size;
+            unsigned int nextEntityY = nextEntityData[edat::EPROP_Y] * edat::quantize_step_size;
+
+            Entity_ExitSwitch* exitSwitch = new Entity_ExitSwitch(gridEntity, nextEntityX, nextEntityY, exitDoor);
+            Helper_RegisterEntity(entities, exitSwitch);
+        }
+        else if (entityType == edat::ETYPE_EXIT_SWITCH) {
+            if (i > 0) {
+                if (entityData[i - 1][edat::EPROP_TYPE] == edat::ETYPE_EXIT_DOOR) {
+                    continue;
+                }
+            }
+        }
+        else if (entityType == edat::ETYPE_FLOORGUARD) {
+            Entity_FloorGuard* floorGuard = new Entity_FloorGuard(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, floorGuard);
+        }
+        else if (entityType == edat::ETYPE_GOLD) {
+            Entity_Gold* gold = new Entity_Gold(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, gold);
+        }
+        else if (entityType == edat::ETYPE_LASER) {
+            Entity_Drone_Laser* laserDrone = new Entity_Drone_Laser(gridEntity, entityX, entityY, Helper_Editor_NewDirEnumToOldDirEnum(entityDir), entityMove);
+            Helper_RegisterEntity(entities, laserDrone);
+        }
+        else if (entityType == edat::ETYPE_LAUNCHPAD) {
+            vec2 directionVector = edat::MAP_DIR_TO_VEC(entityDir);
+            Entity_Launchpad* launchpad = new Entity_Launchpad(gridEntity, entityX, entityY, directionVector.x, directionVector.y);
+            Helper_RegisterEntity(entities, launchpad);
+        }
+        else if (entityType == edat::ETYPE_MINE) {
+            Entity_Mine* mine = new Entity_Mine(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, mine);
+        }
+        else if (entityType == edat::ETYPE_ONEWAY) {
+            vec2 directionVector = edat::MAP_DIR_TO_VEC(entityDir);
+            Entity_OnewayPlatform* onewayPlatform = new Entity_OnewayPlatform(gridEntity, entityX, entityY, directionVector.x, directionVector.y);
+            Helper_RegisterEntity(entities, onewayPlatform);
+        }
+        else if (entityType == edat::ETYPE_PLAYER) {
+            ninjaSpawnLocations.push_back(vec2(entityX, entityY));
+        }
+        else if (entityType == edat::ETYPE_ROCKET) {
+            Entity_Rocket* rocket = new Entity_Rocket(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, rocket);
+        }
+        else if (entityType == edat::ETYPE_SWITCH_LOCKED) {
+            if (i > 0) {
+                if (entityData[i - 1][edat::EPROP_TYPE] == edat::ETYPE_DOOR_LOCKED) {
+                    continue;
+                }
+            }
+        }
+        else if (entityType == edat::ETYPE_SWITCH_TRAP) {
+            if (i > 0) {
+                if (entityData[i - 1][edat::EPROP_TYPE] == edat::ETYPE_DOOR_TRAP) {
+                    continue;
+                }
+            }
+        }
+        else if (entityType == edat::ETYPE_THWOMP) {
+            vec2 directionVector = edat::MAP_DIR_TO_VEC(entityDir);
+            double fallDirection = 0;
+            bool isHorizontal = false;
+
+            if (directionVector.y == 0) {
+                fallDirection = directionVector.x;
+                isHorizontal = true;
+            } else {
+                fallDirection = directionVector.y;
+            }
+
+            Entity_Thwomp* thwomp = new Entity_Thwomp(gridEntity, entityX, entityY, fallDirection, isHorizontal);
+            Helper_RegisterEntity(entities, thwomp);
+        }
+        else if (entityType == edat::ETYPE_TURRET) {
+            Entity_Turret* turret = new Entity_Turret(gridEntity, entityX, entityY);
+            Helper_RegisterEntity(entities, turret);
+        }
+        else if (entityType == edat::ETYPE_ZAP) {
+            Entity_Drone_Zap* zapDrone = new Entity_Drone_Zap(gridEntity, entityX, entityY, Helper_Editor_NewDirEnumToOldDirEnum(entityDir), entityMove);
+            Helper_RegisterEntity(entities, zapDrone);
+        }
+    }
+}
+
+void Helper_RegisterEntity(vector<Entity_Base*>& entities, Entity_Base* entity) {
+    entity->GAME_SetUID(entities.size());
+    entities.push_back(entity);
+}
+
+unsigned int sim_loader::Helper_Editor_OldDirEnumToNewDirEnum(int oldDirEnum) {
+    return oldDirEnum * 2;
+}
+
+int sim_loader::Helper_Editor_NewDirEnumToOldDirEnum(unsigned int newDirEnum) {
+    return newDirEnum / 2;
+}
+
+unsigned int sim_loader::Helper_Editor_VecToDirEnum(double x, double y) {
+    if (x == 0) {
+        if (y == 0) {
+            return 0;
+        }
+        if (y < 1) {
+            return edat::DIR_U;
+        }
+        return edat::DIR_D;
+    }
+    if (y == 0) {
+        if (x == 0) {
+            return 0;
+        }
+        if (x < 1) {
+            return edat::DIR_L;
+        }
+        return edat::DIR_R;
+    }
+    if (x < 0) {
+        if (y < 0) {
+            return edat::DIR_LU;
+        }
+        return edat::DIR_LD;
+    }
+    if (y < 0) {
+        return edat::DIR_RU;
+    }
+    return edat::DIR_RD;
+}
--- simulation/sim_loader.h
+++ simulation/sim_loader.h
@@ -1,31 +1,56 @@
 #pragma once
 #include <vector>
 
+class Editor_State {public: const vector<unsigned int>& tileIDs; const vector<vector<unsigned int>>& entities;};
 #include "..\\editor\\edat.h"
 #include "..\\flash\\utils\\ByteArray.h"
 #include "..\\math\\mathutils.h"
 #include "..\\math\\vec2.h"
-
+class SimpleInput;
 #include "collision\\Grid_Edges.h"
 #include "collision\\Grid_Entity.h"
 #include "collision\\Grid_Segment.h"
 #include "collision\\Segment.h"
-//#include "collision\\Segment_Linear_DoubleSided.h"
+#include "collision\\Segment_Linear_DoubleSided.h"
 #include "entities\\Entity_Base.h"
-//#include "entities\\Entity_ExitDoor.h"
-//#include "entities\\Entity_ExitSwitch.h"
+#include "entities\\Entity_BounceBlock.h"
+#include "entities\\Entity_Door_Locked.h"
+#include "entities\\Entity_Door_Regular.h"
+#include "entities\\Entity_Door_Trap.h"
+#include "entities\\Entity_Drone_Chaingun.h"
+#include "entities\\Entity_Drone_Chaser.h"
+#include "entities\\Entity_Drone_Laser.h"
+#include "entities\\Entity_Drone_Zap.h"
+#include "entities\\Entity_ExitDoor.h"
+#include "entities\\Entity_ExitSwitch.h"
+#include "entities\\Entity_FloorGuard.h"
+#include "entities\\Entity_Gold.h"
+#include "entities\\Entity_Launchpad.h"
+#include "entities\\Entity_Mine.h"
+#include "entities\\Entity_OnewayPlatform.h"
+#include "entities\\Entity_Rocket.h"
+#include "entities\\Entity_Thwomp.h"
+#include "entities\\Entity_Turret.h"
+class InputSource_Base;
+class InputSource_Playback;
+class InputSource_Recorder;
 #include "ninja\\Ninja.h"
-//#include "..\\tiles\\tiledefs.h"
+#include "..\\tiles\\tiledefs.h"
 #include "..\\tiles\\tiletypes.h"
 
 using namespace std;
 
-class Editor_State;
-class SimpleInput;
-class InputSource_Base;
-
 class sim_loader {
     public:
         sim_loader() = delete;
-        Simulator LoadLevel_EditorState(const vector<int>& playerKeys, const vector<unsigned int>& playerColors, const SimpleInput& input, const ByteArray& replayData, int playerCount, const Editor_State& editorState);
+        static Simulator LoadLevel_EditorState(const vector<int>& playerKeys, const vector<unsigned int>& playerColors, const SimpleInput& input, const ByteArray& replayData, int playerCount, const Editor_State& editorState);
+    private:
+        static void LoadLevel_EditorState_Tiles(const vector<unsigned int>& tileIDs, vector<int>& tiles, Grid_Segment& gridSegment, Grid_Edges& gridEdges, int numCols, int numRows, double cellSize, double cellHalfWidth);
+        static void LoadLevel_BuildTileSegs(Grid_Segment& gridSegment, double cellSize, double cellHalfWidth, int colIndex, int rowIndex, int tileType, const vector<int>& neighborTiles);
+        static void LoadLevel_BuildTileEdges(Grid_Edges& gridEdges, int colIndex, int rowIndex, int tileType);
+        static void LoadLevel_EditorState_Entities(const vector<vector<unsigned int>>& entityData, Grid_Segment& gridSegment, Grid_Edges& gridEdges, Grid_Entity& gridEntity, vector<Entity_Base*>& entities, vector<vec2>& ninjaSpawnLocations, double cellSize, double cellHalfWidth);
+        static void Helper_RegisterEntity(vector<Entity_Base*>& entities, Entity_Base* entity);
+        static unsigned int Helper_Editor_OldDirEnumToNewDirEnum(int oldDirEnum);
+        static int Helper_Editor_NewDirEnumToOldDirEnum(unsigned int newDirEnum);
+        static unsigned int Helper_Editor_VecToDirEnum(double x, double y);
 };
