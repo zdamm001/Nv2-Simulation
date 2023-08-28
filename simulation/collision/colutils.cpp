dea199954c29240f0436a9c12c5f4e6fe61b0bc6
zdamm001 <zdamm001@ucr.edu>
2023-08-28 03:34:37 -0700

    fixed function name and changed Seg to pointer

--- simulation/collision/colutils.cpp
+++ simulation/collision/colutils.cpp
@@ -1,6 +1,6 @@
 #include "colutils.h"
 
-int colutils::getSingleClosestPoint_Signed(Grid_Segment& gridSegment, vec2& point, double radius, vec2& closestPointResult) {
+int colutils::GetSingleClosestPoint_Signed(Grid_Segment& gridSegment, vec2& point, double radius, vec2& closestPointResult) {
     bool isPointBackfacing = false;
     double distanceSquared = 0.0;
     double closestDistanceSquared = 99999999.0;
@@ -9,7 +9,7 @@ int colutils::getSingleClosestPoint_Signed(Grid_Segment& gridSegment, vec2& poin
     gridSegment.GatherCellContentsFromWorldspaceRegion(point.x - radius, point.y - radius, point.x + radius, point.y + radius, segmentList);
 
     for (int i = 0; i < segmentList.size(); ++i) {
-        isPointBackfacing = segmentList[i].GetClosestPoint_IsBackfacing(point, closestPoint);
+        isPointBackfacing = segmentList[i]->GetClosestPoint_IsBackfacing(point, closestPoint);
         double deltaX = closestPoint.x - point.x;
         double deltaY = closestPoint.y - point.y;
         distanceSquared = deltaX * deltaX + deltaY * deltaY;
--- simulation/collision/colutils.h
+++ simulation/collision/colutils.h
@@ -3,18 +3,17 @@
 
 #include "..\\..\\math\\vec2.h"
 #include "Grid_Segment.h"
+#include "Segment.h"
 
 using namespace std;
-
-class Segment {};
 
 class colutils {
     private:
         static vec2 closestPoint;
-        static vector<Segment> segmentList;
+        static vector<Segment*> segmentList;
     public:
         colutils() = delete;
-        static int getSingleClosestPoint_Signed(Grid_Segment& gridSegment, vec2& point, double radius, vec2& closestPointResult);
+        static int GetSingleClosestPoint_Signed(Grid_Segment& gridSegment, vec2& point, double radius, vec2& closestPointResult);
         static double Penetration_Square_vs_Point(const vec2& squareCenter, double contactRadius, const vec2& pointPosition, vec2& penetrationNormal);
         static bool Overlap_Circle_Vs_Segment(const vec2& circleCenter, double circleRadius, const vec2& segmentStart, const vec2& segmentEnd, double segmentLength);
         static bool Overlap_Circle_Vs_Circle(const vec2& center1, double radius1, const vec2& center2, double radius2);
