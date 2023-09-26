e5a41bf6c1ddb9130fd0d6ef60d7bf6abde266d4
zdamm001 <zdamm001@ucr.edu>
2023-09-26 16:44:44 -0700

    Added and completed Entity_Launchpad

---
+++ simulation/entities/Entity_Launchpad.cpp
@@ -0,0 +1,52 @@
+#include "Entity_Launchpad.h"
+
+Entity_Launchpad::Entity_Launchpad(Grid_Entity& entities, double x, double y, double nx, double ny)
+    : pos(x, y), n(nx, ny), r(12 * 0.5), strength(12 * (3 / 7)), gfx_triggerEvent(false) {
+    entities.ENTITY_Add(pos, this);
+}
+
+bool Entity_Launchpad::CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) {
+    if (colutils::Overlap_Circle_Vs_Circle(pos, r, circlePosition, circleRadius)) {
+        double deltaX = pos.x - (circlePosition.x - n.x * circleRadius);
+        double deltaY = pos.y - (circlePosition.y - n.y * circleRadius);
+        epsilon = 0.1;
+        if (-epsilon <= n.x * deltaX + n.y * deltaY) {
+            double scaleFactor = 1;
+
+            if (n.y < 0) {
+                scaleFactor += abs(n.y);
+            }
+
+            if (ninja == nullptr) {
+                result.vec_x = n.x * 12;
+                result.vec_y = n.y * 12;
+                gfx_triggerEvent = true;
+                return true;
+            }
+
+            sim->Event_Launchpad_HitPlayer(ninja, n.x * strength, n.y * strength * scaleFactor);
+            gfx_triggerEvent = true;
+        }
+    }
+
+    return false;
+}
+
+EntityGraphics* Entity_Launchpad::GenerateGraphicComponent() {
+    //return new EntityGraphics_Launchpad(this, pos.x, pos.y, atan2(n.y, n.x));
+}
+
+void Entity_Launchpad::GFX_UpdateState(EntityGraphics_Launchpad* graphic) {
+    if (gfx_triggerEvent) {
+        //graphic->anim = EntityGraphics_Launchpad::ANIM_LAUNCH;
+        gfx_triggerEvent = false;
+    } else {
+        //graphic->anim = EntityGraphics_Launchpad::ANIM_IDLE;
+    }
+}
+
+void Entity_Launchpad::Debug_Draw(SimpleRenderer& rend) {
+    //rend.SetStyle(0, 0, 100);
+    //rend.DrawBox(pos.x, pos.y, -n.y * r, n.x * r, n.x * 2, n.y * 2);
+    //rend.DrawLine(pos.x, pos.y, pos.x + n.x * 6, pos.y + n.y * 6);
+}
---
+++ simulation/entities/Entity_Launchpad.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Entity_Base.h"
+//#include "..\\..\\audiovisual\\entitygraphics\\EntityGraphics_Launchpad.h"
+#include "..\\..\\math\\vec2.h"
+//#include "simpleFramework\\SimpleRenderer.h"
+#include "..\\Simulator.h"
+#include "..\\collision\\Grid_Entity.h"
+#include "..\\collision\\colutils.h"
+#include "..\\ninja\\Ninja.h"
+#include "..\\sim_globals.h"
+class EntityGraphics_Launchpad;
+
+class Entity_Launchpad : public Entity_Base {
+    private:
+        vec2 pos;
+        vec2 n;
+        double r;
+        double strength;
+        double pow;
+        bool gfx_triggerEvent;
+    public:
+        Entity_Launchpad(Grid_Entity& entities, double x, double y, double nx, double ny);
+        bool CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) override;
+        EntityGraphics* GenerateGraphicComponent() override;
+        void GFX_UpdateState(EntityGraphics_Launchpad* graphic);
+        void Debug_Draw(SimpleRenderer& rend) override;
+};
