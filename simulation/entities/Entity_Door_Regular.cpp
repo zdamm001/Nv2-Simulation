b1ae8d7214458273aa5f2738f57861569b7707e9
zdamm001 <zdamm001@ucr.edu>
2023-09-26 16:34:10 -0700

    Added and completed Entity_Door_Regular

---
+++ simulation/entities/Entity_Door_Regular.cpp
@@ -0,0 +1,40 @@
+#include "Entity_Door_Regular.h"
+
+Entity_Door_Regular::Entity_Door_Regular(Grid_Entity& entities, Grid_Segment& segments, int segmentIndex, Segment* segment, Grid_Edges& edges, vector<int>& edgeIndices, bool isHorizontal, double x, double y)
+    : close_timer(0), Entity_Door_Base(entities, segments, segmentIndex, segment, edges, edgeIndices, isHorizontal, x, y, 12 * (5 / 6), false) {
+}
+
+void Entity_Door_Regular::OnCollision(Simulator* sim) {
+    close_timer = 0;
+    if (!IsDoorOpen()) {
+        ChangeDoorState(true);
+    }
+}
+
+void Entity_Door_Regular::Think(Simulator* sim) {
+    if (IsDoorOpen()) {
+        ++close_timer;
+        if (close_threshold < close_timer) {
+            ChangeDoorState(false);
+        }
+    }
+}
+
+EntityGraphics* Entity_Door_Regular::GenerateGraphicComponent() {
+    vec2 doorPos = GetDoorPos();
+    //return new EntityGraphics_Door_Regular(this, doorPos.x, doorPos.y, GetDoorOrn());
+}
+
+void Entity_Door_Regular::GFX_UpdateState(EntityGraphics_Door_Regular* graphic) {
+    if (IsDoorOpen()) {
+        //graphic->anim = EntityGraphics_Door_Regular::ANIM_OPEN;
+    } else {
+        //graphic->anim = EntityGraphics_Door_Regular::ANIM_CLOSE;
+    }
+}
+
+void Entity_Door_Regular::Debug_Draw(SimpleRenderer& rend) {
+    Debug_Draw_Base(rend, true);
+    //rend.SetStyle(4, 0x228822, 10);
+    seg->DebugDraw_NoStyle(rend);
+}
---
+++ simulation/entities/Entity_Door_Regular.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Entity_Door_Base.h"
+//#include "..\\..\\audiovisual\\entitygraphics\\EntityGraphics_Door_Regular.h"
+#include "..\\..\\math\\vec2.h"
+//#include "..\\simpleFramework\\SimpleRenderer.h"
+#include "..\\Simulator.h"
+#include "..\\collision\\Grid_Edges.h"
+#include "..\\collision\\Grid_Entity.h"
+#include "..\\collision\\Grid_Segment.h"
+#include "..\\collision\\Segment.h"
+class EntityGraphics_Door_Regular;
+
+class Entity_Door_Regular : public Entity_Door_Base {
+    private:
+        static const int close_threshold = 5;
+        int close_timer;
+    public:
+        Entity_Door_Regular(Grid_Entity& entities, Grid_Segment& segments, int segmentIndex, Segment* segment, Grid_Edges& edges, vector<int>& edgeIndices, bool isHorizontal, double x, double y);
+        void OnCollision(Simulator* sim) override;
+        void Think(Simulator* sim) override;
+        EntityGraphics* GenerateGraphicComponent() override;
+        void GFX_UpdateState(EntityGraphics_Door_Regular* graphic);
+        void Debug_Draw(SimpleRenderer& rend) override;
+};
