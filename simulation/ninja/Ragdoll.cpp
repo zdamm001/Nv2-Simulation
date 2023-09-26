61732bbadf5bafcca2fcc52573c31c7d14b10da3
zdamm001 <zdamm001@ucr.edu>
2023-09-26 16:53:37 -0700

    started Ragdoll

---
+++ simulation/ninja/Ragdoll.cpp
@@ -0,0 +1,5 @@
+#include "Ragdoll.h"
+
+void Ragdoll::GFX_UpdateState(EntityGraphics_Ninja* graphic) {
+
+}
---
+++ simulation/ninja/Ragdoll.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class EntityGraphics_Ninja;
+
+class Ragdoll {
+    public:
+        void GFX_UpdateState(EntityGraphics_Ninja* graphic);
+};
