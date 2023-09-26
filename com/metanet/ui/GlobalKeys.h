9b158b4bbef4ede109ad14e39b28d364fbe1bea5
zdamm001 <zdamm001@ucr.edu>
2023-09-26 16:09:01 -0700

    Added GlobalKeys

---
+++ com/metanet/ui/GlobalKeys.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class GlobalKeys {
+    public:
+        unsigned int up = 38;
+        unsigned int down = 40;
+        unsigned int left = 37;
+        unsigned int right = 39;
+        unsigned int select = 90;
+        unsigned int back = 27;
+        unsigned int pause = 80;
+        unsigned int quit = 81;
+    public:
+        GlobalKeys() {};
+};
