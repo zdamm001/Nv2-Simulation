2c174bc89549a8697745bbd4c914e05ac870edd1
zdamm001 <zdamm001@ucr.edu>
2023-09-26 16:41:56 -0700

    Added and completed Entity_ExitSwitch

---
+++ simulation/entities/Entity_ExitSwitch.cpp
@@ -0,0 +1,38 @@
+#include "Entity_ExitSwitch.h"
+
+Entity_ExitSwitch::Entity_ExitSwitch(Grid_Entity& entities, double x, double y, Entity_ExitDoor* door)
+    : pos(x, y), r(12 * 0.5), door(door) {
+    entities.ENTITY_Add(pos, this);
+}
+
+bool Entity_ExitSwitch::CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) {
+    if (ninja != nullptr) {
+        if (colutils::Overlap_Circle_Vs_Circle(pos, r, circleVelocity, circleRadius)) {
+            door->SWITCH_OpenTheExit(sim->objGrid);
+            sim->objGrid.ENTITY_Remove(this);
+        }
+    }
+    return false;
+}
+
+EntityGraphics* Entity_ExitSwitch::GenerateGraphicComponent() {
+    //return new EntityGraphics_ExitSwitch(this, pos.x, pos.y);
+}
+
+void Entity_ExitSwitch::GFX_UpdateState(EntityGraphics_ExitSwitch* graphic) {
+    if (door->SWITCH_IsOpen()) {
+        //graphic->anim = EntityGraphics_ExitSwitch::ANIM_OPEN;
+    } else {
+        //graphic->anim = EntityGraphics_ExitSwitch::ANIM_CLOSED;
+    }
+}
+
+void Entity_ExitSwitch::Debug_Draw(SimpleRenderer& rend) {
+    if (door->SWITCH_IsOpen()) {
+        //rend.SetStyle(0, 0, 10);
+    } else {
+        //rend.SetStyle(0, 0, 100);
+    }
+    //rend.DrawSquare(pos.x, pos.y, r);
+    //rend.DrawCross(pos.x, pos.y, r);
+}
---
+++ simulation/entities/Entity_ExitSwitch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Entity_Base.h"
+//#include "..\\..\\audiovisual\\entitygraphics\\EntityGraphics_ExitSwitch.h"
+#include "..\\..\\math\\vec2.h"
+//#include "simpleFramework\\SimpleRenderer.h"
+#include "..\\Simulator.h"
+#include "..\\collision\\Grid_Entity.h"
+#include "..\\collision\\colutils.h"
+#include "..\\ninja\\Ninja.h"
+#include "Entity_ExitDoor.h"
+class EntityGraphics_ExitSwitch;
+
+class Entity_ExitSwitch : public Entity_Base {
+    private:
+        vec2 pos;
+        double r;
+        Entity_ExitDoor* door;
+    public:
+        Entity_ExitSwitch(Grid_Entity& entities, double x, double y, Entity_ExitDoor* door);
+        bool CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) override;
+        EntityGraphics* GenerateGraphicComponent() override;
+        void GFX_UpdateState(EntityGraphics_ExitSwitch* graphic);
+        void Debug_Draw(SimpleRenderer& rend) override;
+};
