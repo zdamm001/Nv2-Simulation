ec4c4c926adf874b07f165632b896ed4d58327db
zdamm001 <zdamm001@ucr.edu>
2024-01-29 16:39:31 -0800

    added save functions to Entity_Door_Locked

--- simulation/entities/Entity_Door_Locked.cpp
+++ simulation/entities/Entity_Door_Locked.cpp
@@ -4,6 +4,11 @@ Entity_Door_Locked::Entity_Door_Locked(Grid_Entity& entities, Grid_Segment& segm
     : Entity_Door_Base(entities, segments, segmentIndex, segment, edges, edgeIndices, isHorizontal, x, y, 12.0 * (5.0 / 12.0), false) {
 }
 
+Entity_Door_Locked::Entity_Door_Locked(Grid_Entity& entities, entitySave& entity, Grid_Segment& segments, Grid_Edges& edges)
+    : Entity_Door_Base(entities, entity, segments, edges, 12.0 * (5.0 / 12.0)) {
+    if (entity.is == false) entities.ENTITY_Add(trigger_pos, this);
+}
+
 void Entity_Door_Locked::OnCollision(Simulator* sim) {
     sim->objGrid.ENTITY_Remove(this);
     ChangeDoorState(true);
@@ -11,6 +16,7 @@ void Entity_Door_Locked::OnCollision(Simulator* sim) {
 
 EntityGraphics* Entity_Door_Locked::GenerateGraphicComponent() {
     vec2 doorPos = GetDoorPos();
+    return nullptr;
     //return new EntityGraphics_Door_Locked(this, doorPos.x, doorPos.y, GetDoorOrn(), trigger_pos.x, trigger_pos.y);
 }
 
@@ -27,4 +33,9 @@ void Entity_Door_Locked::Debug_Draw(SimpleRenderer& rend) {
     Debug_Draw_Base(rend, !IsDoorOpen());
     //rend->SetStyle(4, 0x222288, 10);
     seg->DebugDraw_NoStyle(rend);
+}
+
+void Entity_Door_Locked::saveState(entitySave& state) {
+    Entity_Door_Base::saveState(state);
+    state.etype = edat::ETYPE_DOOR_LOCKED;
 }
--- simulation/entities/Entity_Door_Locked.h
+++ simulation/entities/Entity_Door_Locked.h
@@ -14,10 +14,12 @@ class EntityGraphics_Door_Locked;
 class Entity_Door_Locked : public Entity_Door_Base {
     public:
         Entity_Door_Locked(Grid_Entity& entities, Grid_Segment& segments, int segmentIndex, Segment* segment, Grid_Edges& edges, vector<int>& edgeIndices, bool isHorizontal, double x, double y);
+        Entity_Door_Locked(Grid_Entity& entities, entitySave& entity, Grid_Segment& segments, Grid_Edges& edges);
     protected:
         void OnCollision(Simulator* sim) override;
     public:
         EntityGraphics* GenerateGraphicComponent() override;
         void GFX_UpdateState(EntityGraphics* graphic);
         void Debug_Draw(SimpleRenderer& rend) override;
+        void saveState(entitySave& state) override;
 };
