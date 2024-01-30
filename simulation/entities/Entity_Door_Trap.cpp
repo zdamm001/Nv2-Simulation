fb84b8e057c4b409fc594fe9e777947f027ab97a
zdamm001 <zdamm001@ucr.edu>
2024-01-29 16:39:58 -0800

    added save functions to Entity_Door_Trap

--- simulation/entities/Entity_Door_Trap.cpp
+++ simulation/entities/Entity_Door_Trap.cpp
@@ -4,6 +4,11 @@ Entity_Door_Trap::Entity_Door_Trap(Grid_Entity& entities, Grid_Segment& segments
     : Entity_Door_Base(entities, segments, segmentIndex, segment, edges, edgeIndices, isHorizontal, x, y, 12.0 * (5.0 / 12.0), true) {
 }
 
+Entity_Door_Trap::Entity_Door_Trap(Grid_Entity& entities, entitySave& entity, Grid_Segment& segments, Grid_Edges& edges)
+    : Entity_Door_Base(entities, entity, segments, edges, 12.0 * (5.0 / 12.0)) {
+    if (entity.is == true) entities.ENTITY_Add(trigger_pos, this);
+}
+
 void Entity_Door_Trap::OnCollision(Simulator* sim) {
     sim->objGrid.ENTITY_Remove(this);
     ChangeDoorState(false);
@@ -11,6 +16,7 @@ void Entity_Door_Trap::OnCollision(Simulator* sim) {
 
 EntityGraphics* Entity_Door_Trap::GenerateGraphicComponent() {
     vec2 doorPos = GetDoorPos();
+    return nullptr;
     //return new EntityGraphics_Door_Trap(this, doorPos.x, doorPos.y, GetDoorOrn(), trigger_pos.x, trigger_pos.y);
 }
 
@@ -27,4 +33,9 @@ void Entity_Door_Trap::Debug_Draw(SimpleRenderer& rend) {
     Debug_Draw_Base(rend, IsDoorOpen());
     //rend.SetStyle(4, 0x882222, 10);
     seg->DebugDraw_NoStyle(rend);
+}
+
+void Entity_Door_Trap::saveState(entitySave& state) {
+    Entity_Door_Base::saveState(state);
+    state.etype = edat::ETYPE_DOOR_TRAP;
 }
