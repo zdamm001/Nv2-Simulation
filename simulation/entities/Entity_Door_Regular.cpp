72a25084623d5858592de5dfdb3824b8c41550ea
zdamm001 <zdamm001@ucr.edu>
2024-01-29 16:40:29 -0800

    added save functions to Entity_Door_Regular

--- simulation/entities/Entity_Door_Regular.cpp
+++ simulation/entities/Entity_Door_Regular.cpp
@@ -1,7 +1,12 @@
 #include "Entity_Door_Regular.h"
 
 Entity_Door_Regular::Entity_Door_Regular(Grid_Entity& entities, Grid_Segment& segments, int segmentIndex, Segment* segment, Grid_Edges& edges, vector<int>& edgeIndices, bool isHorizontal, double x, double y)
-    : close_timer(0), Entity_Door_Base(entities, segments, segmentIndex, segment, edges, edgeIndices, isHorizontal, x, y, 12 * (5 / 6), false) {
+    : close_timer(0), Entity_Door_Base(entities, segments, segmentIndex, segment, edges, edgeIndices, isHorizontal, x, y, 12.0 * (5.0 / 6.0), false) {
+}
+
+Entity_Door_Regular::Entity_Door_Regular(Grid_Entity& entities, entitySave& entity, Grid_Segment& segments, Grid_Edges& edges)
+    : close_timer(entity.state), Entity_Door_Base(entities, entity, segments, edges, 12.0 * (5.0 / 6.0)) {
+    entities.ENTITY_Add(trigger_pos, this);
 }
 
 void Entity_Door_Regular::OnCollision(Simulator* sim) {
@@ -22,6 +27,7 @@ void Entity_Door_Regular::Think(Simulator* sim) {
 
 EntityGraphics* Entity_Door_Regular::GenerateGraphicComponent() {
     vec2 doorPos = GetDoorPos();
+    return nullptr;
     //return new EntityGraphics_Door_Regular(this, doorPos.x, doorPos.y, GetDoorOrn());
 }
 
@@ -37,4 +43,10 @@ void Entity_Door_Regular::Debug_Draw(SimpleRenderer& rend) {
     Debug_Draw_Base(rend, true);
     //rend.SetStyle(4, 0x228822, 10);
     seg->DebugDraw_NoStyle(rend);
+}
+
+void Entity_Door_Regular::saveState(entitySave& state) {
+    Entity_Door_Base::saveState(state);
+    state.etype = edat::ETYPE_DOOR_REGULAR;
+    state.state = close_timer;
 }
--- simulation/entities/Entity_Door_Regular.h
+++ simulation/entities/Entity_Door_Regular.h
@@ -17,9 +17,11 @@ class Entity_Door_Regular : public Entity_Door_Base {
         int close_timer;
     public:
         Entity_Door_Regular(Grid_Entity& entities, Grid_Segment& segments, int segmentIndex, Segment* segment, Grid_Edges& edges, vector<int>& edgeIndices, bool isHorizontal, double x, double y);
+        Entity_Door_Regular(Grid_Entity& entities, entitySave& entity, Grid_Segment& segments, Grid_Edges& edges);
         void OnCollision(Simulator* sim) override;
         void Think(Simulator* sim) override;
         EntityGraphics* GenerateGraphicComponent() override;
         void GFX_UpdateState(EntityGraphics_Door_Regular* graphic);
         void Debug_Draw(SimpleRenderer& rend) override;
+        void saveState(entitySave& state) override;
 };
