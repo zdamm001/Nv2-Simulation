f47ba9d470fec0247e7d61e843c91826ac94b175
zdamm001 <zdamm001@ucr.edu>
2024-01-29 16:47:38 -0800

    added save functions to Entity_Mine

--- simulation/entities/Entity_Mine.cpp
+++ simulation/entities/Entity_Mine.cpp
@@ -1,8 +1,13 @@
 #include "Entity_Mine.h"
 
 Entity_Mine::Entity_Mine(Grid_Entity& entities, double x, double y)
-    : pos(x, y), r(12 * (1 / 3)), isExploded(false) {
+    : pos(x, y), r(12.0 * (1.0 / 3.0)), isExploded(false) {
     entities.ENTITY_Add(pos, this);
+}
+
+Entity_Mine::Entity_Mine(Grid_Entity& entities, entitySave& entity)
+    : pos(entity.pos), r(6), isExploded(entity.is) {
+    if (!isExploded) entities.ENTITY_Add(pos, this);
 }
 
 bool Entity_Mine::CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) {
@@ -26,6 +31,7 @@ bool Entity_Mine::CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collisio
 }
 
 EntityGraphics* Entity_Mine::GenerateGraphicComponent() {
+    return nullptr;
     //return new EntityGraphics_Mine(this, pos.x, pos.y);
 }
 
@@ -45,4 +51,20 @@ void Entity_Mine::Debug_Draw(SimpleRenderer& rend) {
     }
     //rend.DrawCross(pos.x, pos.y, r);
     //rend.DrawCircle(pos.x, pos.y, r);
+}
+
+// ByteArray Entity_Mine::saveState() {
+//     ByteArray state;
+//     state.writeByte(edat::STRUCTTYPE_MINE);
+//     state.writeDouble(pos.x);
+//     state.writeDouble(pos.y);
+//     state.writeBoolean(isExploded);
+//     return state;
+// }
+
+void Entity_Mine::saveState(entitySave& state) {
+    Entity_Base::saveState(state);
+    state.etype = edat::ETYPE_MINE;
+    state.pos = pos;
+    state.is = isExploded;
 }
--- simulation/entities/Entity_Mine.h
+++ simulation/entities/Entity_Mine.h
@@ -18,8 +18,11 @@ class Entity_Mine : public Entity_Base {
         bool isExploded;
     public:
         Entity_Mine(Grid_Entity& entities, double x, double y);
+        Entity_Mine(Grid_Entity& entities, entitySave& entity);
         bool CollideVsCircle_Logical(Simulator* sim, Ninja* ninja, collision_result_logical& result, const vec2& circlePosition, const vec2& circleVelocity, const vec2& circleOldPosition, double circleRadius, double epsilon) override;
         EntityGraphics* GenerateGraphicComponent() override;
         void GFX_UpdateState(EntityGraphics_Mine* graphic);
         void Debug_Draw(SimpleRenderer& rend) override;
+        // ByteArray saveState() override;
+        void saveState(entitySave& state) override;
 };
